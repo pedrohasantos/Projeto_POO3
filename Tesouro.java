59f3a0000beea8c5d6698fd243285002e00f1a72
pedrohasantos <133005154+pedrohasantos@users.noreply.github.com>
2024-05-25 11:56:51 -0300

    Update Tesouro.java

--- Tesouro.java
+++ Tesouro.java
@@ -13,39 +13,11 @@ public class Tesouro {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public int getPosicao() {
         return posicao;
     }
 
-    public void setPosicao(int posicao) {
-        this.posicao = posicao;
-    }
-
     public int getPontos() {
         return pontos;
-    }
-
-    public void setPontos(int pontos) {
-        this.pontos = pontos;
-    }
-
-    public void efeito() {
-        // Efeito padrão do tesouro, pode ser sobrescrito por subclasses
-    }
-}
-
-class TesouroEspecial extends Tesouro {
-    public TesouroEspecial(String nome, int posicao, int pontos) {
-        super(nome, posicao, pontos);
-    }
-
-    @Override
-    public void efeito() {
-        // Implementar efeito especial
-        System.out.println("Efeito especial ativado!");
     }
 }
